ea6fa2ec7ce5b7ded9a26131ad180cb0d29a5684
Conrad Makow <cmakow@gmail.com>
2017-07-17 12:12:50 -0400

    Added YT bottom half to main dashboard page

--- frontend/components/youtube/youtube.jsx
+++ frontend/components/youtube/youtube.jsx
@@ -7,6 +7,7 @@ class Youtube extends React.Component {
   }
 
   componentDidMount() {
+    //sets dashboard as active component in state, highlighting button on navbar
     this.props.setDashboardActive();
   }
 
@@ -27,6 +28,28 @@ class Youtube extends React.Component {
           <p className='ytSkipConnect'>
             Skip
           </p>
+        </div>
+        <div className='ytBotHalf'>
+          <div className='ytAddChannel'>
+            <div className='ytSmallHeader'>
+              Manually Add YouTube <br />
+              Channel URL
+            </div>
+            <div className='ytSubText'>
+              Or enter keywords to search for a channel
+            </div>
+            <input type='text' className='ytInput' placeholder='http://www.youtube.com/user/PandoraMusic' />
+          </div>
+          <div className='ytAddPlaylist'>
+            <div className='ytSmallHeader'>
+              Manually Add YouTube <br />
+              Playlist URL
+            </div>
+            <div className='ytSubText'>
+              Paste the URL from any song within a YouTube Playlist
+            </div>
+            <input type='text' className='ytInput' placeholder='http://www.youtube.com/watch?v=oWYp1xRPH5g&list...' />
+          </div>
         </div>
       </div>
     )
